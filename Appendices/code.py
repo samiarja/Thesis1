e552cc651487a7fa3e5af7dc3f52dfadeb0ca9a3
Sami Arja <sami18040571@outlook.com>
2019-06-08 00:08:29 +1000

    Initial Overleaf Import

---
+++ Appendices/code.py
@@ -0,0 +1,1 @@
+keras.layers.Conv3D(filters, kernel_size, strides=(1, 1, 1), padding='valid', data_format=None, dilation_rate=(1, 1, 1), activation=None, use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)
